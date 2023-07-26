5aed57c5b2131d002ba04e6adad743ccaaa9b7eb
Yoshy0777 <josiaspuga@gmail.com>
2023-07-26 10:41:03 -0600

    QRGeneradoPDF

--- Funcion_PDF.py
+++ Funcion_PDF.py
@@ -1,12 +1,28 @@
 from reportlab.pdfgen import canvas
+from Funcion_QR import *
 from reportlab.lib.pagesizes import A4
 
 ruta = "C:/Users/FRANKILIJOSHY/Desktop/Modularidad en python/prueba funciones/"
 nombreArchivo = ruta + "Ejemplo de funciones.pdf"
+nombreQR = ruta + "miQR.png"
 
-def GenerarPDF():
+def GenerarPDF(listaNombres,listaEdades):
+    generarQR(nombreQR,"Hola desde funcion")
     c = canvas.Canvas(nombreArchivo)
     c.setFont('Helvetica', 17)
-    c.drawString(240,520,"PDF generado con funciones")
+    Xinicial = 200
+    Yinicial = 700
+
+    c.drawString(Xinicial,Yinicial,"Edad: ")
+    c.drawString(Xinicial+140,Yinicial,"Nombre: ")
+
+    c.drawImage(nombreQR,200,400,100,100)
+    for i in range(len(listaNombres)):
+        c.setFont('Helvetica',20)
+        Yinicial = Yinicial -20        
+        c.drawString(Xinicial,Yinicial,listaEdades[i])        
+        c.drawString(Xinicial+140,Yinicial,listaNombres[i])
+
+
     c.save()
-    print("Reporte generado")
+    print("--------Reporte generado-------")
--- Funcion_QR.py
+++ Funcion_QR.py
@@ -1,7 +1,8 @@
 import qrcode
 
-ruta = "C:/Users/FRANKILIJOSHY/Desktop/Modularidad en python/prueba funciones/"
-img = qrcode.make("Ya llego su mero gallo")
-nombreImagen = ruta + "miQR.png"
-f = open(nombreImagen, "wb")
-img.save(f)
+def generarQR(nombreQR, informacion):
+
+    img = qrcode.make(informacion)
+    f = open(nombreQR, "wb")
+    img.save(f)
+    f.close()
